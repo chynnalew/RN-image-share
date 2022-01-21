47db0031307d706b2381ca77b4e1625402f24bf9
Chynna Lew <chynnalew@yahoo.com>
2022-01-21 13:08:45 -0800

    add button

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello world</Text>
-      <StatusBar style="auto" />
+      <Image source={{uri:"https://i.imgur.com/TkIrScD.png"}} style={styles.logo} />
+      <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
+
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +19,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 350,
+    height: 159,
+    marginBottom: 10,
+  },
+    instructions:{
+      color: '#888',
+      fontSize: 18,
+      marginHorizontal: 15,
+  },
+  button: {
+    fontSize: 20,
+    backgroundColor: '#888',
+    padding: 20,
+    borderRadius: 5
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff'
+  }
 });
